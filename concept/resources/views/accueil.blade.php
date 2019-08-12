5dc63569f1ca5fd5dca01cf4ae08bac12bedc2b5
BertSaulnier <bertrand.saulnier@hotmail.com>
2019-08-12 15:40:47 -0400

    Ajout de d'une vue de base (copie Multimédia)

---
+++ concept/resources/views/accueil.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('contenu')
+    <h1>Accueil</h1>
+    <h3>
+		Quelque chose ici
+    </h3>
+@endsection
---
+++ concept/resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>420-219-SF</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+ 
+    <!-- Le styles -->
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <span class="label label-info">420-219-SF</span><h1>Développement multimédia</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+       @yield('contenu')
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+	<script>
+	</script>
+  </body>
+</html>
