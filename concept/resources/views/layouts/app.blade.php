fe3d38bdda0cb5d4d6124044e57782e855f1c239
BertSaulnier <bertrand.saulnier@hotmail.com>
2019-08-13 11:31:30 -0400

    Modification du lien bootstrap, problèmes avec la navbar

--- concept/resources/views/layouts/app.blade.php
+++ concept/resources/views/layouts/app.blade.php
@@ -2,24 +2,35 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title>420-219-SF</title>
+    <title>Les restaurants Concept!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
  
     <!-- Le styles -->
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
 
   <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-dark bg-dark fixed-top">
       <div class="navbar-inner">
-        <div class="container">
-          <span class="label label-info">420-219-SF</span><h1>Développement multimédia</h1>
+        <div class="container-fluid">
+			<div class="row justify-content-between">
+				<div class="col-4">
+					<div>
+						<a href="/"><h1>Les restaurants Concept!</h1></a>
+					</div>
+				</div>
+				<div class="col-4">
+					<div>
+						<h2>Bonjour</h2>
+					</div>
+				</div>
+			</div>
         </div>
       </div>
-    </div>
+    </nav>
     <div class="container">
        @yield('contenu')
     </div> <!-- /container -->
