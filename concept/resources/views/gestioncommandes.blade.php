05ce41f6e9b9307fc6d6aad5b95085a05c0770e6
BertSaulnier <bertrand.saulnier@hotmail.com>
2019-08-20 15:52:18 -0400

    Exemple de connexion à la base de données

--- concept/resources/views/gestioncommandes.blade.php
+++ concept/resources/views/gestioncommandes.blade.php
@@ -9,7 +9,22 @@
 		</div>
 	</div>
     
-    <h3>
-		Quelque chose ici
-    </h3>
+    <table class="table table-striped table-bordered">
+    <thead>
+      <tr>
+        <th class="col-md-4 text-center">Numéro</th>
+        <th class="col-md-8">Date</th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach($commandes as $commande)
+      <tr>
+        <td class="text-center">{{ $commande['idCommande'] }}</td>
+        <td>
+          <strong>{{ $commande['datecommande'] }}</strong>
+        </td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
 @endsection
--- concept/routes/web.php
+++ concept/routes/web.php
@@ -1,5 +1,33 @@
 <?php
 
+function obtenirConnexion()
+{
+    $PARAM_hote='localhost'; 
+    $PARAM_port='3306';
+    $PARAM_utilisateur='root'; 
+    $PARAM_nom_bd= 'resto_concept'; 
+    $PARAM_mot_passe=''; 
+
+    try
+    {
+       $connexion = new PDO(
+           'mysql:host=' . $PARAM_hote . ';port=' . $PARAM_port .
+           ';dbname=' . $PARAM_nom_bd, 
+           $PARAM_utilisateur, $PARAM_mot_passe);
+       $connexion->exec("SET CHARACTER SET utf8");
+       $mode = PDO::FETCH_ASSOC; // Collection d'association
+       //$mode = PDO::FETCH_OBJ;   // Objet PHP
+       $connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $mode); 
+
+       return $connexion;
+    } catch(Exception $e) 
+      {
+        echo 'Erreur : '.$e->getMessage().'<br>';
+        echo 'N° : '.$e->getCode();
+        die();
+    }    
+}
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -20,5 +48,13 @@ $app->get('/connexion', function () use ($app) {
 });
 
 $app->get('/gestioncommandes', function () use ($app) {
-	return view('gestioncommandes');
+	$connexion = obtenirConnexion();
+    $requete = $connexion->query(
+        'SELECT * ' .
+        'FROM commandes ' .
+        'WHERE idetat = 1');
+    $commandes = $requete->fetchAll();
+    $requete->closeCursor();
+    $connexion = null;
+	return view('gestioncommandes', ['commandes' => $commandes]);
 });
