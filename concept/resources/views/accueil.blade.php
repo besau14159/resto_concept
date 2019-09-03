1538ea8cc00a15a287a49c8804275cb6229cea07
BertSaulnier <bertrand.saulnier@hotmail.com>
2019-09-03 17:23:58 -0400

    changements mineurs pour le style

--- concept/resources/views/accueil.blade.php
+++ concept/resources/views/accueil.blade.php
@@ -4,5 +4,6 @@
 	<div class="row justify-content-center">
 		<h1 class="text-align-center">Bienvenue aux restaurants Concept!</h1>
 		<img class="img-fluid" src="../images/PoutineAccueil.jpg"></img>
+		<h3>Cinq sites à Québec pour mieux vous servir</h3>
 	</div>
 @endsection
